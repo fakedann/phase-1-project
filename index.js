4b9da86bc5e1e68c02911351c44b6f2921a3621c
Daniel Escalona <daniel07escalona@gmail.com>
2022-03-07 15:37:06 -0500

    Start button creates new sections and html form to initialize search. 2ND TRY

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const body = document.querySelector('body')
+  
+  const btn = document.querySelector('.btn')
+  btn.addEventListener('click', function(){
+    const summary = document.createElement('div')
+    summary.className = 'summary'
+    let p = document.createElement('p')
+    p.innerHTML = "Search your favorite films down below."
+    summary.appendChild(p)
+    body.appendChild(summary)
+
+    const main = document.createElement('main')
+    body.appendChild(main)
+    let form = document.createElement('form')
+    form.innerHTML = `<h4>Add New Film</h4>
+    <label for="title">Title: </label>
+    <input type="text" name="title" id="new-film" />
+    <label for="director">Director: </label>
+    <input type="text" name="director" id="new-director" />
+    <input type="submit" value="Create" />`
+    main.appendChild(form)
+  }, {once : true})
+  
+});
+
+function startFetching(){
   let input = 'the irishman'
   let year = '2019'
   console.log(input+' '+year)
-
-
   fetch(`https://imdb-api.com/en/API/SearchMovie/k_652gzlhp/${input} ${year}`)
   .then(resp => resp.json() )
   .then(resp => handleSearch(resp.results))
-  
-});
+}
 
 function handleSearch(findings){
   console.log(findings)
